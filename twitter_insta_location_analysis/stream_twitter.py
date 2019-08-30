2c13eb53de3b05d7b70603d089c87f021a3795b3
Shantanu Bhattacharyya <heisenberg@Shantanus-MacBook-Pro.local>
2019-08-30 15:52:14 +0530

    removed info

--- twitter_insta_location_analysis/stream_twitter.py
+++ twitter_insta_location_analysis/stream_twitter.py
@@ -4,21 +4,6 @@ from twython import TwythonStreamer
 import json
 import time
 
-#twitter = Twython('aGtrJA30BA50LGtZmdAc9zmJ3', 'by2EqbNQji9Xm66fcsVKRVt9B3WU6kAkekV3C4IJJb7ocu6w7q','2586262244-9S520P1X2sZW8ajbNyxUBALo1Ea2z17nVLaJZmt', 'yJVMOueRmiW5rce1tWJK40v6YEUMx5LDvQx9UjaBSf8LM')
-
-
-'''
-x = twitter.get_home_timeline()
-for i in x:
-	print(i['text']+"\n")
-
-print ("&%^%&**&(&&*()*)(*()&*(^&%^^%^")
-
-y = twitter.search(q='taylor', result_type='popular', count=200)
-
-for n in range(len(y)):
-	print(y[n])
-'''
 
 class MyStreamer(TwythonStreamer):
 	def on_success(self, data):
@@ -33,7 +18,7 @@ class MyStreamer(TwythonStreamer):
 			print(username + " | " + time + " | "  + tweet_text + " | " + source + " | " + "loc : " + location)
 
 stream = MyStreamer(
-	'aGtrJA30BA50LGtZmdAc9zmJ3', 'by2EqbNQji9Xm66fcsVKRVt9B3WU6kAkekV3C4IJJb7ocu6w7q','2586262244-9S520P1X2sZW8ajbNyxUBALo1Ea2z17nVLaJZmt', 'yJVMOueRmiW5rce1tWJK40v6YEUMx5LDvQx9UjaBSf8LM'
+	'urcred', 'urcred','urcred', 'urcred'
 )
 
 count_tweets = 0
